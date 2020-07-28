bec653806d1b074135d46fd8ec4c37336dc20e2d
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-07-28 17:27:18 +0000

    refactoring to make code wrapper by a function

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
-sentence.split("").forEach((letter, idx) => {
-  setTimeout(() => {
-    process.stdout.write(letter);
-    if (idx >= sentence.length - 1) {
-      process.stdout.write("\n");
-    }
-  }, 50 * idx);
-});
+const typewriter = (stringInput) => {
+  stringInput.split("").forEach((letter, idx) => {
+    setTimeout(() => {
+      process.stdout.write(letter);
+      if (idx >= stringInput.length - 1) {
+        process.stdout.write("\n");
+      }
+    }, 50 * idx);
+  });
+};
+
+typewriter(sentence);
